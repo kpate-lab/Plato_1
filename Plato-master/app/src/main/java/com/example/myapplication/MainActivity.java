72e7d24d42df0cac4c329e8c95fa461d64a78a7f
kpate-lab <kpatel3099@gmail.com>
2020-11-05 18:07:17 -0500

    Initial commit

---
+++ Plato-master/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabBar);
+        TabItem tabCards = findViewById(R.id.tabFlashcards);
+        TabItem tabCalendar = findViewById(R.id.tabCalendar);
+        TabItem tabNotes = findViewById(R.id.tabNotes);
+        TabItem tabPomodoro = findViewById(R.id.tabPomodoro);
+        final ViewPager viewPager = findViewById((R.id.viewPager));
+
+        PagerAdapter pagerAdapter = new
+                PagerAdapter(getSupportFragmentManager(),
+                tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        }
+    }
